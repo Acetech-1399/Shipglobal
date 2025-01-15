c879760f4de1403b178944349497d3251a0af690
Khan <ibadkaleem18@gmail.com>
2025-01-15 15:50:32 +0530

    Added global address functionality

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Mailbox
+from .models import *
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -10,3 +10,8 @@ class UserAdmin(admin.ModelAdmin):
 class MailboxAdmin(admin.ModelAdmin):
     list_display = ("item_name", "product_value", "tracking_number", "user")
     search_fields = ("item_name", "tracking_number")
+
+@admin.register(GlobalAddress)
+class GlobaAddressAdmin(admin.ModelAdmin):
+    list_display = ("country", "state", "zip_code", "phone")
+    search_fields = ("country", "state", "zip_code")
